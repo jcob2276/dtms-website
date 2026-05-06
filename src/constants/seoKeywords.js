66e4e8b1ee2564c774d3b7fb3e57bb4eab080925
Jakub <jakubsobon3@gmail.com>
2026-05-06 14:36:13 +0200

    chore: add B2B SEO keywords

--- src/constants/seoKeywords.js
+++ src/constants/seoKeywords.js
@@ -4,14 +4,16 @@ export const HIDDEN_SEMANTICS = {
     "Kursy zawodowe Podkarpacie",
     "Uprawnienia państwowe UDT",
     "Egzaminy UDT Krosno",
-    "Certyfikowane szkolenia operatorów",
+    "Szkolenia grupowe dla firm",
+    "Kursy UDT dla pracowników",
     "DTMS Krosno",
-    "Szkoła zawodowa Krosno",
-    "Kursy UDT Jasło Sanok Brzozów"
+    "Szkolenia techniczne dla przemysłu",
+    "Ośrodek szkolenia UDT Podkarpacie"
   ],
   categories: {
     wozki: [
       "Kurs na wózki widłowe Krosno",
+      "Szkolenie operatorów dla firm",
       "Wózki jezdniowe specjalizowane I WJO",
       "Ładowarki teleskopowe szkolenie",
       "Wózki unoszące i platformowe",
@@ -19,30 +21,33 @@ export const HIDDEN_SEMANTICS = {
     ],
     podesty: [
       "Kurs na zwyżki Krosno",
+      "Podesty ruchome dla pracowników",
       "Podesty ruchome przejezdne szkolenie",
       "Zwyżki i nożycówki uprawnienia",
-      "Podnośniki koszowe operator"
+      "Operator podnośników koszowych"
     ],
     zurawie: [
       "Kurs HDS Krosno",
       "Żurawie wieżowe i samojezdne",
+      "Szkolenia dla operatorów dźwigów",
       "Dźwigi budowlane i towarowe",
-      "Wyciągi towarowe i windy",
-      "Żurawie stacjonarne warsztatowe"
+      "Wyciągi towarowe i windy"
     ],
     suwnice: [
       "Operator suwnic Krosno",
       "Szkolenie hakowy sygnalista",
+      "Suwnice szkolenia grupowe",
       "Wciągniki i wciągarki UDT",
       "Zapinacz hakowy kurs"
     ],
     napelnianie: [
       "Napełnianie butli gazami Krosno",
-      "Uprawnienia ODO Straż Pożarna",
-      "Napełnianie butli nurkowych",
+      "Szkolenia ODO dla Straży Pożarnej",
+      "Uprawnienia napełnianie butli nurkowych",
       "Gazy techniczne i skroplone kurs"
     ]
   }
 };
+
 
 
