83173d6262ccb50b91cfc284c389c169775aa5fb
Jakub <jakubsobon3@gmail.com>
2026-05-06 15:10:26 +0200

    feat: implement PWA support

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'dtms-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Instalacja Service Workera
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Aktywacja i czyszczenie starych cache'y
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Obsługa zapytań (Cache First, then Network)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
